81af6b260dbd61c5139894ae7f578d55bc71ae0b
lamtota40 <lamtota40@gmail.com>
2023-05-21 13:54:45 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    echo "You can Try comand 'su root' or 'sudo -i' or 'sudo -'"
+    exit 1
+fi
+
 apt update
 apt upgrade -y
 apt install iodine
