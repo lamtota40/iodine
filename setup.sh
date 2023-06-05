3b713690a2e2390626683a2070b4282d81581ab2
lamtota40 <lamtota40@gmail.com>
2023-06-05 09:28:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ apt install libz-dev
 iodined -c -f 10.0.0.1 -P pass ns.sit.my.id
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sysctl -p
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t filter -A FORWARD -i eth0 -o dns0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t filter -A FORWARD -i dns0 -o eth0 -j ACCEPT
